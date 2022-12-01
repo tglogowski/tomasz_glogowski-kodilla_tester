4faef9c30b8cdb1cae1b3875837bc3eecdc7b768
Tomasz Glogowski <tglogowski@dinox.pl>
2022-12-01 20:38:43 +0100

    Pierwsze kroki

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Tomasz");
+        System.out.println("43 lata");
+        System.out.println('G');
+    }
+}
