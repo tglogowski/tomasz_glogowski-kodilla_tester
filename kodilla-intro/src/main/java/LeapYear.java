7ae3a8d2a1f1e1f506aa6ab30597920c0b451aaf
Tomasz Glogowski <tglogowski@dinox.pl>
2022-12-04 14:32:38 +0100

    Zadanie 4

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj rok: ");
+        int yearToCheck = scanner.nextInt();
+        boolean isLeap = false;
+
+            if (yearToCheck%4 == 0 && yearToCheck%100 == 0 && yearToCheck%400 == 0){
+                isLeap = true;
+            } else if (yearToCheck%4 == 0 && yearToCheck%100 != 0){
+            isLeap = true;
+            }
+
+            if (isLeap == true){
+                System.out.print("Rok ");
+                System.out.print(yearToCheck);
+                System.out.println(" jest przestępny.");
+            } else {
+                System.out.print("Rok ");
+                System.out.print(yearToCheck);
+                System.out.println(" nie jest przestępny.");
+            }
+    }
+}
+
