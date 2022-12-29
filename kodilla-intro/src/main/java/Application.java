1f5d6723329c9b3b82b634998f50e3eb51a727a7
Tomasz Glogowski <tglogowski@dinox.pl>
2022-12-29 20:23:29 +0100

    TAX-2.6 Poprawka #1

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-       Human person1 = new Human("Adam", 40.5,178);
-       person1.checkParams();
+       Human human = new Human("Adam", 40.5,178);
+       human.checkParams();
     }
 }
--- kodilla-intro/src/main/java/Human.java
+++ kodilla-intro/src/main/java/Human.java
@@ -1,16 +1,15 @@
 public class Human {
-    String name;
-    double age1;
-    double height1;
+    private String name;
+    private double age;
+    private double height;
 
-    public Human(String name, double age, double height){
-
+    public Human(String name, double age, double height) {
         this.name = name;
-        this.age1 = age;
-        this.height1 = height;
+        this.age = age;
+        this.height = height;
     }
     public void checkParams() {
-        if (age1 > 30 && height1 > 160) {
+        if (age > 30 && height > 160) {
             System.out.println("User is older than 30 and taller than 160cm");
         } else {
             System.out.println("User is 30 (or younger) or 160cm (or shorter)");
