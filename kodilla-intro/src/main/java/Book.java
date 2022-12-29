8a9423ea14398b243913fb60e5439233db49c160
Tomasz Glogowski <tglogowski@dinox.pl>
2022-12-29 20:37:59 +0100

    TAX-2.7 Poprawka #1

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,13 +2,11 @@ class Book {
     private String author;
     private String title;
 
-    public static Book of(String author, String title){
-            Book books = new Book();
-            books.author = author;
-            books.title = title;
-        System.out.println("Autor książki: " + books.author);
-        System.out.println("Tytuł książki: " + books.title);
-    return books;
+    public static Book of(String author, String title) {
+            Book book = new Book();
+            book.author = author;
+            book.title = title;
+    return book;
     }
 
     public static void main(String[] args) {
