951da5774380b68be080897430909cb639bf2ccc
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-02 23:42:18 +0100

    TAX-2.5 Poprawka #6

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,28 +1,18 @@
 class User {
-    private final String name;
-    private final int age;
+
+    private String name;
+    private int age;
 
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public static double printYoungUsers(User[] user) {
-        int sum = 0;
-
-        if (user.length > 0) {
-            for (int i = 0; i < user.length; i++) {
-                sum += user[i].age;
-            }
-            return (double) sum / user.length;
-        }
-        return 0;
+    public String getName() {
+        return name;
     }
 
-    public static void printNames(User[] user, double ave){
-             for (int j = 0; j < user.length; j++)
-                if (user[j].age < ave) {
-                    System.out.println(user[j].name);
-                }
+    public int getAge() {
+        return age;
     }
 }
--- kodilla-intro/src/main/java/UserApplication.java
+++ kodilla-intro/src/main/java/UserApplication.java
@@ -1,17 +1,37 @@
 public class UserApplication {
-
+    static User[] user = new User[5];
+    static double aveAge;
     public static void main(String[] args) {
 
-        User[] user = new User[5];
+        addUsers();
+        aveAge = calcAveAge();
+        printNames();
+    }
+
+    private static void addUsers(){
 
         user[0] = new User("Peter", 18);
         user[1] = new User("John", 26);
         user[2] = new User("Jenny", 22);
         user[3] = new User("Alice", 32);
         user[4] = new User("Damian", 42);
+    }
 
-        double average = User.printYoungUsers(user);
+    private static double calcAveAge() {
 
-        User.printNames(user, average);
+        int sum = 0;
+        if (user.length > 0) {
+            for (int i = 0; i < user.length; i++) {
+                sum += user[i].getAge();
+            }
+            return (double) sum / user.length;
+        }
+        return 0;
+    }
+    private static void printNames(){
+             for (int j = 0; j < user.length; j++)
+                if (user[j].getAge() < aveAge) {
+                    System.out.println(user[j].getName());
+                }
     }
 }
