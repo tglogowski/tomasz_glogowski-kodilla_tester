26cef1be6c98b299caa12f71c0340858062a7b11
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-05 00:41:37 +0100

    TA-2.8 Poprwka #1

--- kodilla-intro/src/main/java/UserDialogs.java
+++ kodilla-intro/src/main/java/UserDialogs.java
@@ -12,4 +12,26 @@ public class UserDialogs {
             System.out.println("Name is too short. Try again.");
         }
     }
+
+    public static String getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select calcultaion (A-add, S-substract, D-divide, M-multiply):");
+            String calc = scanner.nextLine().trim().toUpperCase();
+            switch (calc) {
+                case "A": return "ADD";
+                case "S": return "SUB";
+                case "D": return "DIV";
+                case "M": return "MUL";
+                default:
+                    System.out.println("Wrong calculation. Try again.");
+            }
+        }
+    }
+    public static int getValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int val = scanner.nextInt();
+        return val;
+    }
 }
