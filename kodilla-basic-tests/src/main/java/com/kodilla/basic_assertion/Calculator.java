85321740ade27fbd2952d487d353cf797ec83ac5
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-05 15:04:46 +0100

    TA-3.5 Wprowadzenie do testów jednostkowych

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/ResultChecker.java
@@ -0,0 +1,8 @@
+package com.kodilla.basic_assertion;
+
+public class ResultChecker {
+
+    public static boolean assertEquals(int expected, int astual) {
+        return expected == astual;
+    }
+}
