924c6c9da382c0ecca54a61acbee1c9877de0fd3
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-09 18:59:34 +0100

    TAX-3.6 Podstawy JUnit - zadanie

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -14,4 +14,37 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
+
+    @Test
+    public void testSubtrack() {
+        Calculator calculator = new Calculator();
+        int a = 21;
+        int b = 7;
+        int subtrackResult = calculator.subtract(a, b);
+        assertEquals(14, subtrackResult);
+    }
+
+    @Test
+    public void testSquareNumber() {
+        Calculator calculator = new Calculator();
+        int a = 2;
+        double squareNumberResult = calculator.squareNumber(a);
+        assertEquals(4, squareNumberResult, 0.1);
+    }
+
+    @Test
+    public void testSquareZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        double squareNumberResult = calculator.squareNumber(a);
+        assertEquals(0, squareNumberResult, 0.1);
+    }
+
+    @Test
+    public void testSquareNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        int a = -3;
+        double squareNumberResult = calculator.squareNumber(a);
+        assertEquals(9, squareNumberResult, 0.1);
+    }
 }
