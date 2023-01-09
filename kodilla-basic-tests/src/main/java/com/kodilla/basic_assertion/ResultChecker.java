aad577886140164650cc44c22989c85e7f1883b9
tglogowski <119533747+tglogowski@users.noreply.github.com>
2023-01-09 18:32:13 +0100

    TAX-3.5 Rozbudowa testów kalkulatora (#18)
    TAX-3.5 Rozbudowa testów kalkulatora

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/ResultChecker.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/ResultChecker.java
@@ -2,7 +2,7 @@ package com.kodilla.basic_assertion;
 
 public class ResultChecker {
 
-    public static boolean assertEquals(int expected, int astual) {
-        return expected == astual;
+    public static boolean assertEquals(int expected, int actual) {
+        return expected == actual;
     }
 }
