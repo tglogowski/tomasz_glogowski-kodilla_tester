fc7c2fa64ccfbd918e6e02d69c9a532abea52378
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-11 00:34:25 +0100

    TAX-4.1 Poprawka #2

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -1,28 +1,26 @@
 package com.kodilla.collections.interfaces.homework;
 
 public class CarRace {
-
-    private static Car[] car = new Car[3];
     private static final int SPEED_DECREMENT_VALUE = 20;
     public static void main(String[] args) {
 
-        car[0] = new Ford(50, SPEED_DECREMENT_VALUE);
-        car[1] = new Jaguar(70, SPEED_DECREMENT_VALUE);
-        car[2] = new Renault(45, SPEED_DECREMENT_VALUE);
+        Car ford = new Ford(50, SPEED_DECREMENT_VALUE);
+        Car jaguar = new Jaguar(70, SPEED_DECREMENT_VALUE);
+        Car renault = new Renault(45, SPEED_DECREMENT_VALUE);
 
-        doRace(car);
+        doRace(ford);
+        doRace(jaguar);
+        doRace(renault);
 
     }
-        private static void doRace(Car[] car) {
+        private static void doRace(Car car) {
 
-            for (int j = 0; j < car.length; j++) {
                 for (int i = 0; i < 3; i++)
-                    car[j].increaseSpeed();
+                    car.increaseSpeed();
 
                 for (int i = 0; i < 2; i++)
-                    car[j].decreaseSpeed();
+                    car.decreaseSpeed();
 
-                System.out.println("Actual " + car[j].getClass().getSimpleName() + " speed: " + car[j].getSpeed());
+                System.out.println("Actual " + car.getClass().getSimpleName() + " speed: " + car.getSpeed());
             }
-        }
 }
