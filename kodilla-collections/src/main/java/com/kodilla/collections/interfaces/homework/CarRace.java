3379224484f60b3a36a090b0349c725a3e5868d3
tglogowski <119533747+tglogowski@users.noreply.github.com>
2023-01-10 12:29:14 +0100

    Update CarRace.java
    Dodałem  brakującą klamrę otwierającą wnętrze metody main

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -2,7 +2,7 @@ package com.kodilla.collections.interfaces.homework;
 
 public class CarRace {
     private static final int SPEED_DECREMENT_VALUE = 10;
-    public static void main(String[] args)
+    public static void main(String[] args){
 
         Car ford = new Ford(20, SPEED_DECREMENT_VALUE);
         ford.increaseSpeed();
