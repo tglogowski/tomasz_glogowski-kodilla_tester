083a5521b7067e1f15ec87f884c442baade8a7ad
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-12 20:32:46 +0100

    TAX-4.3 Zadanie: operacje na listach

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Ford.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Ford.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.interfaces.homework;
 
+import java.util.Objects;
+
 public class Ford implements Car{
 
     private int actualSpeed = 0;
@@ -32,5 +34,18 @@ public class Ford implements Car{
     @Override
     public int getSpeed() {
         return actualSpeed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ford ford = (Ford) o;
+        return speedIncrementValue == ford.speedIncrementValue && speedDecrementValue == ford.speedDecrementValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(speedIncrementValue, speedDecrementValue);
     }
 }
