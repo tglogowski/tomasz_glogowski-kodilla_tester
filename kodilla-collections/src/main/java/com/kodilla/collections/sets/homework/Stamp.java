dd8ac8a9edaf6476f9d2f6f7c47ffad16f6e901a
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-16 20:32:02 +0100

    TAX-4.4 Poprawka #2

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -7,33 +7,12 @@ public class Stamp {
     private double stampLength;
     private double stampHeight;
     private boolean isStamped;
-    private static int[] hashCodes = new int[0];
-    private static int counter = 0;
-    private static int addsCounter = 0;
 
     public Stamp(String stampName, double stampLength, double stampHeight, boolean isStamped) {
         this.stampName = stampName;
         this.stampLength = stampLength;
         this.stampHeight = stampHeight;
         this.isStamped = isStamped;
-        addsCounter++;
-        if (ifNotExistsInCollection())
-            addStamp();
-    }
-
-    public boolean ifNotExistsInCollection() {
-        for (int i = 0; i < counter; i++) {
-            if(hashCodes[i] == hashCode()) return false;
-        }
-        return true;
-    }
-
-    private void addStamp() {
-        this.counter++;
-        int[] tempTab = new int[this.counter];
-        System.arraycopy(hashCodes,0,tempTab,0,hashCodes.length);
-        tempTab[this.counter -1 ] = hashCode();
-        this.hashCodes = tempTab;
     }
 
     public String getStampName() {
@@ -52,10 +31,6 @@ public class Stamp {
         return isStamped;
     }
 
-    public static int getNumberOfUniqueStamps() {
-        return counter;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -67,10 +42,6 @@ public class Stamp {
                 && Double.compare(stamp.stampHeight, stampHeight) == 0
                 && isStamped == stamp.isStamped
                 && Objects.equals(stampName, stamp.stampName);
-    }
-
-    public static int getNumberOfRejected() {
-        return (addsCounter - counter);
     }
 
     @Override
--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -4,33 +4,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StampsApplication {
+    private static int countOfUniqueStamps = 0;
     public static void main(String[] args) {
 
         Set<Stamp> stamps = new HashSet<>();
         initStamps(stamps);
-        browseStamps(stamps);
+        checkNumberOfStamps(stamps);
     }
 
-    private static void browseStamps(Set<Stamp> stamps) {
-        System.out.println("\nNumber of rejected stamps: " + Stamp.getNumberOfRejected());
-        System.out.println("List of stamps: ");
-        int i = 0;
-        System.out.println("--------------------------------------------------------------------------------------------------");
-            if (Stamp.getNumberOfUniqueStamps() == stamps.size())
-                for (Stamp stamp : stamps) {
-                    i++;
-                    System.out.println(i + ". " + stamp);
-                    System.out.println("--------------------------------------------------------------------------------------------------");
-                }
+    private static void checkNumberOfStamps(Set<Stamp> stamps) {
+
+        if (countOfUniqueStamps == stamps.size())
+            System.out.println("Set collection contains " + countOfUniqueStamps + " elements.");
     }
 
     private static void initStamps(Set<Stamp> stamps) {
 
-        stamps.add(new Stamp("Unsinkable Titanic", 30, 23, true));
-        stamps.add(new Stamp("Stranger In The Fog", 35, 22, false));
-        stamps.add(new Stamp("Strong Assets", 32, 25, false));
-        stamps.add(new Stamp("Firm In Their Ambitions", 35, 28, false));
-        stamps.add(new Stamp("Unsinkable Titanic", 30, 23, true));
-        stamps.add(new Stamp("Strong Assets", 32, 25, false));
+        if(stamps.add(new Stamp("Unsinkable Titanic", 30, 23, true))) countOfUniqueStamps++;
+        if(stamps.add(new Stamp("Stranger In The Fog", 35, 22, false))) countOfUniqueStamps++;
+        if(stamps.add(new Stamp("Strong Assets", 32, 25, false))) countOfUniqueStamps++;
+        if(stamps.add(new Stamp("Firm In Their Ambitions", 35, 28, false))) countOfUniqueStamps++;
+        if(stamps.add(new Stamp("Unsinkable Titanic", 30, 23, true))) countOfUniqueStamps++;
+        if(stamps.add(new Stamp("Strong Assets", 32, 25, false))) countOfUniqueStamps++;
     }
 }
