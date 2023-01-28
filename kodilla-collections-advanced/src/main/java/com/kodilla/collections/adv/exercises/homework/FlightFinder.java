ace1f111ea1d094195ad2b69c98aaee7590b9a5a
Tomasz Glogowski <tglogowski@dinox.pl>
2023-01-28 10:29:13 +0100

    TAX-5.4 Poprawka #2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -1,19 +1,39 @@
 package com.kodilla.collections.adv.exercises.homework;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FlightFinder {
 
-    private static final int FROM = 0;
-    private static final int TO = 1;
+    private Map<String, List<Flight>> flightsFrom = new HashMap<>();
+    private Map<String, List<Flight>> flightsTo = new HashMap<>();
+
+    public FlightFinder(List<Flight> flights) {
+//        flightsFrom = flights.stream().collect(Collectors.groupingBy(Flight::getDeparture));
+//        flightsTo = flights.stream().collect(Collectors.groupingBy(Flight::getArrival));
+        for (Flight flight : flights) {
+            if (flightsFrom.containsKey(flight.getDeparture())) {
+                flightsFrom.get(flight.getDeparture()).add(flight);
+            } else {
+                List<Flight> fl = new ArrayList<>();
+                fl.add(flight);
+                flightsFrom.put(flight.getDeparture(), fl);
+            }
+            if (flightsTo.containsKey(flight.getArrival())) {
+                flightsTo.get(flight.getArrival()).add(flight);
+            } else {
+                List<Flight> fl = new ArrayList<>();
+                fl.add(flight);
+                flightsTo.put(flight.getArrival(), fl);
+            }
+        }
+    }
 
     public List<Flight> findFlightsFrom(String departure) {
-
-        return FlightRepository.getFlightsTable(departure, FROM);
+        return flightsFrom.getOrDefault(departure, Collections.emptyList());
     }
 
     public List<Flight> findFlightsTo(String arrival) {
-
-        return FlightRepository.getFlightsTable(arrival, TO);
+        return flightsTo.getOrDefault(arrival, Collections.emptyList());
     }
 }
--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
@@ -1,103 +1,82 @@
 package com.kodilla.collections.adv.exercises.homework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FlightFinderTestSuite {
 
-    FlightRepository flightRepository = new FlightRepository();
-    FlightFinder findFlights = new FlightFinder();
+    private FlightFinder flightFinder;
+
+    @BeforeEach
+    public void setup() {
+        flightFinder = new FlightFinder(FlightRepository.getFlightsTable());
+    }
 
     @Test
     public void testFindFlightsFrom() {
         //given
-        flightRepository.addFlight(new Flight("London", "Warsaw"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "New York"));
-        flightRepository.addFlight(new Flight("Warsaw", "London"));
-
         //when
-        List<Flight> result = findFlights.findFlightsFrom("London");
+        List<Flight> result = flightFinder.findFlightsFrom("London");
 
         //then
-        List<Flight> expectedList = new LinkedList<>();
-        expectedList.add(new Flight("London", "Warsaw"));
-        expectedList.add(new Flight("London", "Rome"));
-        assertEquals(expectedList, result);
+        assertEquals(List.of(new Flight("London", "Warsaw"), new Flight("London", "Rome")), result);
     }
 
     @Test
-    public void testFindFlightsFrom_IfReturnsDuplicateFlights() {
+    public void testFindFlightsFromWhenFlightTableIsEmpty() {
         //given
-        flightRepository.addFlight(new Flight("London", "Warsaw"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "London"));
+        flightFinder = new FlightFinder(List.of());
 
         //when
-        List<Flight> result = findFlights.findFlightsFrom("London");
-        int sizeResult = result.size();
+        List<Flight> result = flightFinder.findFlightsFrom("London");
 
         //then
-        List<Flight> expectedList = new LinkedList<>();
-        expectedList.add(new Flight("London", "Warsaw"));
-        expectedList.add(new Flight("London", "Rome"));
-        int expectedSize = 2;
-        assertEquals(expectedList, result);
-        assertEquals(expectedSize, sizeResult);
+        assertEquals(List.of(), result);
+    }
+
+    @Test
+    public void testFindFlightsFromWhenNotFound() {
+        //given
+        //when
+        List<Flight> result = flightFinder.findFlightsFrom("Chicago");
+
+        //then
+        assertEquals(List.of(), result);
     }
 
     @Test
     public void testFindFlightsTo() {
         //given
-        flightRepository.addFlight(new Flight("London", "Warsaw"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "New York"));
-        flightRepository.addFlight(new Flight("Warsaw", "London"));
-
         //when
-        List<Flight> result = findFlights.findFlightsTo("Rome");
+        List<Flight> result = flightFinder.findFlightsTo("London");
 
         //then
-        List<Flight> expectedList = new LinkedList<>();
-        expectedList.add(new Flight("London", "Rome"));
-        expectedList.add(new Flight("Warsaw", "Rome"));
-        assertEquals(expectedList, result);
+        assertEquals(List.of(new Flight("Warsaw", "London")), result);
     }
 
     @Test
-    public void testFindFlightsTo_IfReturnsDuplicateFlights() {
+    public void testFindFlightsToWhenFlightTableIsEmpty() {
         //given
-        FlightRepository flightRepository = new FlightRepository();
-
-        flightRepository.addFlight(new Flight("London", "Warsaw"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("London", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "Rome"));
-        flightRepository.addFlight(new Flight("Warsaw", "London"));
-        flightRepository.addFlight(new Flight("Los Angeles", "Rome"));
+        flightFinder = new FlightFinder(List.of());
 
         //when
-        List<Flight> result = findFlights.findFlightsTo("Rome");
-        int sizeResult = result.size();
+        List<Flight> result = flightFinder.findFlightsTo("London");
 
         //then
-        List<Flight> expectedList = new LinkedList<>();
-        expectedList.add(new Flight("London", "Rome"));
-        expectedList.add(new Flight("Warsaw", "Rome"));
-        expectedList.add(new Flight("Los Angeles", "Rome"));
-        int expectedSize = 3;
-        assertEquals(expectedList, result);
-        assertEquals(expectedSize, sizeResult);
+        assertEquals(List.of(), result);
+    }
+
+    @Test
+    public void testFindFlightsToWhenNotFound() {
+        //given
+        //when
+        List<Flight> result = flightFinder.findFlightsTo("Chicago");
+
+        //then
+        assertEquals(List.of(), result);
     }
 }
