92b09ed15f368f550ea5b4bab268a580caaeda21
tglogowski <119533747+tglogowski@users.noreply.github.com>
2023-01-28 10:47:52 +0100

    TA-6.1 Stream – programowanie funkcyjne (#42)
    * TAX-5.3 Zadanie: tworzenie niemutowalnej klasy
    
    * TAX-5.3 Poprawka #1
    
    * TA-6.1 Stream  programowanie funkcyjne

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> chemistGroupUserNames = filterChemistsGroupUsernames();
+        System.out.println(chemistGroupUserNames);
+    }
+
+    private static List<String> filterChemistsGroupUsernames() {
+        List<String> usernames = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+
+        return usernames;
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
+    }
+}
