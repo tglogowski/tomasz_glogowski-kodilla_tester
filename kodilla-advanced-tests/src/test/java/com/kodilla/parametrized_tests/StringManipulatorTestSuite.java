60813e2307e7413010235d83b7c22a5468513d00
Tomasz Glogowski <tglogowski@dinox.pl>
2023-02-19 13:13:32 +0100

    TA-7.3 Obiekty jako dane wejsciowe

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,14 +1,28 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringManipulatorTestSuite {
 
     private StringManipulator manipulator = new StringManipulator();
+
+//    private static Stream<Arguments> provideStringsForTestingLength() {
+//        return Stream.of(
+//                Arguments.of("test", 4),
+//                Arguments.of("OtHEr ", 5),
+//                Arguments.of("E V e n t", 5),
+//                Arguments.of("null", 4),
+//                Arguments.of("A", 1)
+//        );
+//    }
 
 //    @ParameterizedTest
 //    @CsvSource(value = {"test,tset", "OtHEr,rehto", "EVent,tneve", "null,llun", "A,a"})
@@ -33,10 +47,22 @@ class StringManipulatorTestSuite {
 //    public void shouldCountNumberOfCommas(String input, int expected) {
 //        assertEquals(expected, manipulator.countNumberOfCommas(input));
 //    }
-
+//
+//    @ParameterizedTest
+//    @CsvSource(value = {"test,4", " OtHEr,5", "E V e n t,5", "null ,4", "A,1"})
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
+//
+//    @ParameterizedTest
+//    @MethodSource(value = "provideStringsForTestingLength")
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr,5", "E V e n t,5", "null ,4", "A,1"})
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSources#provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
     }
+
 }
