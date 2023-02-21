1a96726d55fdcb4d34f716419d8fb96daa3aaf2c
Tomasz Glogowski <tglogowski@dinox.pl>
2023-02-21 13:30:06 +0100

    TA-7.6 Mockito  cwiczenia

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
@@ -0,0 +1,24 @@
+package com.kodilla.mockito;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NotificationService {
+
+    private Set<Client> clients = new HashSet<>();
+
+    public void  addSubscriber(Client client) {
+        this.clients.add(client);
+    }
+
+    public void sendNotification(Notification notification) {
+        this.clients.forEach(client -> client.receive(notification));
+    }
+
+    public void removeSubscriber(Client client) {
+        this.clients.remove(client);
+
+    }
+}
