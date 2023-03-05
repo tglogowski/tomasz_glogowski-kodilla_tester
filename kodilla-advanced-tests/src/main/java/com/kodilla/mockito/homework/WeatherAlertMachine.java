338a27e8fa8c43ad420a670b80544e23b1e9bca0
Tomasz Glogowski <tglogowski@dinox.pl>
2023-03-05 13:14:03 +0100

    TAX-7.6 Poprawka #1

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertMachine.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertMachine.java
@@ -1,69 +1,41 @@
 package com.kodilla.mockito.homework;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class WeatherAlertMachine {
 
     Map<Localization, Set<Subscriber>> subscriptions = new HashMap<>();
 
     public void addSubscriber(Subscriber subscriber, Localization localization) {
-
-        Set<Subscriber> subscribers = subscriptions.getOrDefault(localization, new HashSet<>());
-        subscribers.add(subscriber);
-        subscriptions.put(localization, subscribers);
+        getFromMap(localization).add(subscriber);
     }
 
     public void sendAlert(WeatherAlert weatherAlert, Localization localization) {
-
-        Set<Subscriber> subscribers = subscriptions.getOrDefault(localization, new HashSet<>());
-        this.subscriptions.entrySet().forEach(s -> {
-            if (s.getKey() == localization) {
-                for (Subscriber subscriber : subscribers)
-                    subscriber.receiveAlert(weatherAlert);
-            }
-        });
+        getFromMap(localization)
+                .forEach(subscriber -> subscriber.receiveAlert(weatherAlert));
     }
 
-    public void removeSubscriberFromLocalization(Subscriber subscriber, Localization localization){
-
-        Set<Subscriber> subscribers = subscriptions.getOrDefault(localization, new HashSet<>());
-
-        if(subscribers.contains(subscriber)) {
-            subscribers.remove(subscriber);
-            subscriptions.put(localization, subscribers);
-        }
+    public void removeSubscriberFromLocalization(Subscriber subscriber, Localization localization) {
+        getFromMap(localization).remove(subscriber);
     }
 
     public void removeSubscriberFromAllLocalizations(Subscriber subscriber) {
-
-        for (Map.Entry<Localization, Set<Subscriber>> subs : subscriptions.entrySet()) {
-            Set<Subscriber> subscribers = subscriptions.getOrDefault(subs.getKey(), new HashSet<>());
-            if (subscribers.contains(subscriber)) {
-                    subscribers.remove(subscriber);
-                    subscriptions.put(subs.getKey(), subscribers);
-            }
-        }
+        subscriptions.forEach((key, value) -> value.remove(subscriber));
     }
 
     public void sendNotificationToAll(OtherNotifications notification) {
-
-        Set<Subscriber> allSubscribers = new HashSet<>();
-
-        for (Map.Entry<Localization, Set<Subscriber>> subs : subscriptions.entrySet()) {
-            Set<Subscriber> subscribers = subscriptions.getOrDefault(subs.getKey(), new HashSet<>());
-            for (Subscriber s : subscribers)
-                allSubscribers.add(s);
-            }
-
-        for(Subscriber s : allSubscribers)
-            s.receiveNotification(notification);
+        subscriptions.values().stream()
+                .flatMap(Collection::stream)
+                .distinct()
+                .forEach(x -> x.receiveNotification(notification));
     }
 
     public void removeLocalization(Localization localization) {
-
         subscriptions.remove(localization);
+    }
+
+    private Set<Subscriber> getFromMap(Localization localization) {
+        subscriptions.computeIfAbsent(localization, k -> new HashSet<>());
+        return subscriptions.get(localization);
     }
 }
