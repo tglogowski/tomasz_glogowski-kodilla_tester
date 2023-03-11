f422e89802ecfda04742b4303d0ea694c01e0b29
tglogowski <119533747+tglogowski@users.noreply.github.com>
2023-03-11 20:38:28 +0100

    Ta 9.3 (#69)

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SkypeMessageService.java
@@ -1,9 +1,10 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
-import com.kodilla.spring.basic.dependency_injection.MessageService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class SkypeMessageService implements MessageService {
 
     public String send(String message, String receiver) {
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShippingCenter {
+
+    @Autowired
+    private DeliveryService deliveryService;
+    @Autowired
+    private NotificationService notificationService;
+
+    public String sendPackage(String address, double weight) {
+        if (deliveryService.deliverPackage(address, weight)) {
+            return notificationService.success(address);
+        }
+        return notificationService.fail(address);
+    }
+}
