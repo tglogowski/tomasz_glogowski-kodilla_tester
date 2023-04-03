9692c32b01944a25822711b8fbb81b013b49cd34
tglogowski <119533747+tglogowski@users.noreply.github.com>
2023-04-03 21:17:23 +0200

    TAX-13.3 Zadanie: testy wyszukiwarki (#83)

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EBayTestingApp {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
+        inputField.submit();
+    }
+}
